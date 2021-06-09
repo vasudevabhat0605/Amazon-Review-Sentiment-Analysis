4f288d177cc46c2d9ef45fecf15fab94ceaceaf6
vasudeva <54314865+vasudevabhat0605@users.noreply.github.com>
2021-06-09 18:14:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import pickle
 import joblib
-model = joblib.load('sentiment analysis')
+model = joblib.load("sentiment analysis")
 st.title('Review-Analysis')
 text = st.text_input('Enter the review message')
 out = model.predict([text])
